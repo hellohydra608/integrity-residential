c055b822a0fb9aeff8fffd3157358f8308a8ac2f
Hello Hydra <82913735+hellohydra608@users.noreply.github.com>
2021-04-22 21:16:22 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -30,10 +30,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'kodywright1989@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contact@hellohydra.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nResume: $resume\n\nName: $name\n\nStart Date: $startDate\n\nPosition: $position\n\nDOB: $dob\n\nSSN: $ssn\n\nStreet: $street\n\nCity: $city\n\nState: $state\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@hellohydra.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;
